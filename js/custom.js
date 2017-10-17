686af42d17e1548d8ef73196a2a1df2608bfcb88
tempday <609262343@qq.com>
2017-10-18 02:27:32 +0800

    1018

--- js/custom.js
+++ js/custom.js
@@ -47,18 +47,10 @@
 		})
 	});
 
-	 /*
-	function moveImg(){
-		if($("div.imgitem:eq(0)").css('left')<="-16em"){
-			$(this).css('left',"0em");
-		}
-		$("div.imgitem").animate({
-				left:'-=16em'
-			},1000,'linear',moveImg);
 
-	}
-	*/
-	//图片自动左切换
+})(jQuery);
+
+//图片自动左切换
 	var slidePic={
 		timer:null,
 		interval:200,
@@ -67,15 +59,16 @@
 		margin:1,
 		unit:"em",
 		tag:$(".inlineblock>ul"),
-		moveImg:function(){
+		liPos:null,
+		moveLeft:function(){
 			var ob=this;
 			ob.tag.animate({
-				marginLeft:'-=1.6em'
+				left:'-=1' + ob.unit
 			},ob.interval,"linear",function(){
 				ob.count++;
-				if(ob.count==10){
+				if(ob.count==16){
 					ob.count=0;
-					ob.tag.css({marginLeft:0});
+					ob.tag.css({left:0});
 					ob.tag.children(":first").appendTo(ob.tag);
 					ob.tag.children().each(function(i) {
 						$(this).css({
@@ -83,25 +76,42 @@
 						})
 					})
 				}
-			})
+			});
+			ob.timer&&(clearTimeout(ob.timer));
+			ob.timer=setTimeout(function(){
+				ob.moveLeft();
+			},ob.interval);
+			 /*
+			 ob.count++;
+			ob.tag.children().each(function(i) {
+				$(this).animate({left:'-=1' + ob.unit},ob.interval,"linear")
+			});
+			ob.timer&&(clearTimeout(ob.timer));
+			ob.timer=setTimeout(function(){
+				if(ob.count==16){
+					ob.count=0;
+					ob.tag.children().eq(0).stop().css({left:5*16+"em"}).appendTo(ob.tag);
+				}
+				ob.moveLeft();
+			},ob.interval);
+
+			//return true;
+			*/
 		},
 		startToMove:function(){
 			var ob=this;
-			ob.moveImg();
-			ob.timer=setInterval(function(){
-				ob.moveImg();
-			},ob.interval);
-			ob.tag.mouseover(function(){
-				clearInterval(ob.timer);
+			ob.liPos=ob.tag.children().eq(1).css('left');
+			ob.moveLeft();
+			ob.tag.mouseenter(function(){
+				clearTimeout(ob.timer)
 				ob.timer=null;
 			});
-			ob.tag.mouseout(function(){
-				ob.timer=setInterval(function(){
-					ob.moveImg()
-				},ob.interval);
+			//out 在子元素之间切换也会触发
+			ob.tag.mouseleave(function(){
+				ob.moveLeft();
 			});
+
 		}
 	};
 	slidePic.startToMove();
-
-})(jQuery);
+	window.slidePic=slidePic;
